0f6d860c911fc0ab97d443b7a189a390596cad86
Rolian Sarmiento <rjsarmiento@student.hau.edu.ph>
2026-03-05 19:13:20 +0800

    added code on models, serializers and views in the authentication app in backend

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer, MyTokenObtainPairSerializer
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {"message": "User created."}, 
+            status=status.HTTP_201_CREATED
+        )
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
